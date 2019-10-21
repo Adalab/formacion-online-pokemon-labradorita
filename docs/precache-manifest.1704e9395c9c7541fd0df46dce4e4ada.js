a055a87ddbe2742c8de596908981fef36ff7aa66
rmend <mendizraquel@gmail.com>
2019-10-21 14:21:15 +0200

    docs?

---
+++ docs/precache-manifest.1704e9395c9c7541fd0df46dce4e4ada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441cb04a9e07cf0463bce762592fabda",
+    "url": "./index.html"
+  },
+  {
+    "revision": "994cee532ea3a85fb468",
+    "url": "./static/css/main.685cd1e5.chunk.css"
+  },
+  {
+    "revision": "8325c7b3e55495fd7d55",
+    "url": "./static/js/2.8eb9ecf9.chunk.js"
+  },
+  {
+    "revision": "994cee532ea3a85fb468",
+    "url": "./static/js/main.f2237c4b.chunk.js"
+  },
+  {
+    "revision": "919e458a1b5a61a83437",
+    "url": "./static/js/runtime-main.6ba19833.js"
+  },
+  {
+    "revision": "5bf8782b78cd1c8bccc17aa03cf20f18",
+    "url": "./static/media/pokemonLogo.5bf8782b.png"
+  }
+]);
